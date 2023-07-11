f44fd678c67081502aff3764c0fc060fb4b46043
Erick <erick.sanchez,ponce@gmail.com>
2023-07-10 20:41:03 -0600

    primer commit

---
+++ Rep/code.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAdXeP7jseYbT4utE0lnwisIoj555C2D14",
+    authDomain: "pruebafirebaseerick.firebaseapp.com",
+    projectId: "pruebafirebaseerick",
+    storageBucket: "pruebafirebaseerick.appspot.com",
+    messagingSenderId: "269496135848",
+    appId: "1:269496135848:web:3b11247483b22f203b929d"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
