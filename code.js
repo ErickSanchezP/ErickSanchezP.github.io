f19d5d3c785d9dd24c617671d6abaa2339ecb2e8
Erick <erick.sanchez,ponce@gmail.com>
2023-07-25 16:23:11 -0600

    diseño pag principal y mas archivos

--- code.js
+++ code.js
@@ -1,19 +1,9 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("Sidenav").style.width = "250px";
+}
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyAdXeP7jseYbT4utE0lnwisIoj555C2D14",
-    authDomain: "pruebafirebaseerick.firebaseapp.com",
-    projectId: "pruebafirebaseerick",
-    storageBucket: "pruebafirebaseerick.appspot.com",
-    messagingSenderId: "269496135848",
-    appId: "1:269496135848:web:3b11247483b22f203b929d"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("Sidenav").style.width = "0";
+}
