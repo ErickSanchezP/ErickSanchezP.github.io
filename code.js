208febf7850b41c622e66b277537534c6c238ac8
Erick <erick.sanchez,ponce@gmail.com>
2023-08-07 04:58:30 -0600

    ajustes footer, pagina principal y animacion menu

--- code.js
+++ code.js
@@ -1,17 +1,36 @@
+
+const openMenu = document.querySelector ("#openMenu");
+let displacement = 30;
 /* Set the width of the side navigation to 250px */
 function openNav() {
   
-  document.getElementById("Sidenav").style.width = "250px";
+  document.getElementById("sidenav").style.width = "350px";
+  document.getElementById("nvl1").style.opacity = "1";
+  document.getElementById("nvl2").style.opacity = "1";
+  document.getElementById("nvl3").style.opacity = "1";
+  document.getElementById("nvl4").style.opacity = "1";
+  document.getElementById("nvl5").style.opacity = "1";
+  document.getElementById("nvl1").style.transform = `translateX(${displacement}px)`;
+  document.getElementById("nvl2").style.transform = `translateX(${displacement}px)`;
+  document.getElementById("nvl3").style.transform = `translateX(${displacement}px)`;
+  document.getElementById("nvl4").style.transform = `translateX(${displacement}px)`;
+  document.getElementById("nvl5").style.transform = `translateX(${displacement}px)`;
+
 }
-
-const navbarlist = document.querySelector(".navbar-link");
-const openMenu = document.querySelector ("#openMenu");
-
-openMenu.addEventListener("click", () => {
-  navbarlist.classList.toggle("slideIn");
-});
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-  document.getElementById("Sidenav").style.width = "0";
-} 
+  document.getElementById("sidenav").style.width = "0";
+
+  document.getElementById("nvl1").style.opacity = "0";
+  document.getElementById("nvl2").style.opacity = "0";
+  document.getElementById("nvl3").style.opacity = "0";
+  document.getElementById("nvl4").style.opacity = "0";
+  document.getElementById("nvl5").style.opacity = "0";
+  document.getElementById("nvl1").style.transform = `translateX(-${displacement}px)`;
+  document.getElementById("nvl2").style.transform = `translateX(-${displacement}px)`;
+  document.getElementById("nvl3").style.transform = `translateX(-${displacement}px)`;
+  document.getElementById("nvl4").style.transform = `translateX(-${displacement}px)`;
+  document.getElementById("nvl5").style.transform = `translateX(-${displacement}px)`;
+
+}
