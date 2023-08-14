cde4c79c1b964f8a5599f1a1a81aaeff54fa4fa1
Erick <erick.sanchez,ponce@gmail.com>
2023-08-14 10:32:32 -0600

    conversiones 140823

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 
-const openMenu = document.querySelector ("#openMenu");
+
 let displacement = 30;
 /* Set the width of the side navigation to 250px */
 function openNav() {
@@ -10,27 +10,51 @@ function openNav() {
   document.getElementById("nvl3").style.opacity = "1";
   document.getElementById("nvl4").style.opacity = "1";
   document.getElementById("nvl5").style.opacity = "1";
+  document.getElementById("nvl6").style.opacity = "1";
+  document.getElementById("nvl7").style.opacity = "1";
+  document.getElementById("nvl8").style.opacity = "1";
+  document.getElementById("nvl9").style.opacity = "1";
+  document.getElementById("nvl10").style.opacity = "1";
   document.getElementById("nvl1").style.transform = `translateX(${displacement}px)`;
   document.getElementById("nvl2").style.transform = `translateX(${displacement}px)`;
   document.getElementById("nvl3").style.transform = `translateX(${displacement}px)`;
   document.getElementById("nvl4").style.transform = `translateX(${displacement}px)`;
   document.getElementById("nvl5").style.transform = `translateX(${displacement}px)`;
+  document.getElementById("nvl6").style.transform = `translateX(${displacement}px)`;
+  document.getElementById("nvl7").style.transform = `translateX(${displacement}px)`;
+  document.getElementById("nvl8").style.transform = `translateX(${displacement}px)`;
+  document.getElementById("nvl9").style.transform = `translateX(${displacement}px)`;
+  document.getElementById("nvl10").style.transform = `translateX(${displacement}px)`;
 
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-  document.getElementById("sidenav").style.width = "0";
 
+  
+  document.getElementById("sidenav").style.width = "0";
+  
   document.getElementById("nvl1").style.opacity = "0";
   document.getElementById("nvl2").style.opacity = "0";
   document.getElementById("nvl3").style.opacity = "0";
   document.getElementById("nvl4").style.opacity = "0";
   document.getElementById("nvl5").style.opacity = "0";
+  document.getElementById("nvl6").style.opacity = "0";
+  document.getElementById("nvl7").style.opacity = "0";
+  document.getElementById("nvl8").style.opacity = "0";
+  document.getElementById("nvl9").style.opacity = "0";
+  document.getElementById("nvl10").style.opacity = "0";
+  document.getElementById("nvl11").style.opacity = "0";
   document.getElementById("nvl1").style.transform = `translateX(-${displacement}px)`;
   document.getElementById("nvl2").style.transform = `translateX(-${displacement}px)`;
   document.getElementById("nvl3").style.transform = `translateX(-${displacement}px)`;
   document.getElementById("nvl4").style.transform = `translateX(-${displacement}px)`;
   document.getElementById("nvl5").style.transform = `translateX(-${displacement}px)`;
+  document.getElementById("nvl6").style.transform = `translateX(-${displacement}px)`;
+  document.getElementById("nvl7").style.transform = `translateX(-${displacement}px)`;
+  document.getElementById("nvl8").style.transform = `translateX(-${displacement}px)`;
+  document.getElementById("nvl9").style.transform = `translateX(-${displacement}px)`;
+  document.getElementById("nvl10").style.transform = `translateX(-${displacement}px)`;
+  document.getElementById("nvl11").style.transform = `translateX(-${displacement}px)`;
 
-}
+};
